223a8da656658a12e23cb0c754558b30a34e798c
Emmanuel Arias <ariassotoemmanuel@gmail.com>
2018-12-01 07:48:22 -0600

    Day 01 Puzzle 02

--- Day01/Day01.cpp
+++ Day01/Day01.cpp
@@ -3,17 +3,51 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <unordered_set>
 
 using std::string;
 
 int main()
 {
+	std::vector<int> inputList;
 	int frequency{ 0 };
 	int inputNumber;
 
 	while (std::cin >> inputNumber) {
-		frequency += inputNumber;
+		inputList.push_back(inputNumber);
 	}
-	std::cout << "Frequency is: " << frequency << '\n';
+
+	/* find final freq (puzzle 1)
+	for (const auto n : inputList) {
+		frequency += n;
+	}
+	*/
+
+	// find first repeated frequency, need to iterate the list 1+ times
+	std::unordered_set<int> foundFreq;
+	auto listIt = inputList.begin();
+	for (;;) {
+		// Alter freq
+		frequency += *listIt;
+
+		if (foundFreq.find(frequency) != foundFreq.end()) {
+			// Repeated freq found
+			break;
+		}
+		else {
+			// Add to lookup set
+			foundFreq.insert(frequency);
+		}
+
+		listIt++;
+		// Make it circular
+		if (listIt == inputList.end()) {
+			listIt = inputList.begin();
+		}
+	}
+
+
+	std::cout << "Repeated frequency is: " << frequency << '\n';
 	return 0;
 }
