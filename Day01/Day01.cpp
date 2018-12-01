d6155351d49df28bc931f8f6e7c24820b1f6a3b8
Emmanuel Arias <ariassotoemmanuel@gmail.com>
2018-12-01 07:27:37 -0600

    Day01 puzzle 1

---
+++ Day01/Day01.cpp
@@ -0,0 +1,19 @@
+﻿// Day01.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include <string>
+
+using std::string;
+
+int main()
+{
+	int frequency{ 0 };
+	int inputNumber;
+
+	while (std::cin >> inputNumber) {
+		frequency += inputNumber;
+	}
+	std::cout << "Frequency is: " << frequency << '\n';
+	return 0;
+}
