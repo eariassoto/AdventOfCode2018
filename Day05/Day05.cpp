792c683860293cf11e52ca2b96cda4c092379f53
Emmanuel Arias <ariassotoemmanuel@gmail.com>
2018-12-05 15:58:11 -0600

    day 05 puzzle 02

--- Day05/Day05.cpp
+++ Day05/Day05.cpp
@@ -6,8 +6,30 @@
 #include <fstream>
 #include <stack>
 #include <ctype.h>
+#include <limits>
+#include <algorithm>
+#include <unordered_set>
 
 using std::string;
+
+size_t calculatePolymerUnits(const string &input) {
+	std::stack<char> polymerStack;
+
+	for (const char ch : input) {
+		if (polymerStack.empty()) {
+			polymerStack.push(ch);
+			continue;
+		}
+		char top = polymerStack.top();
+		if (top != ch && (tolower(top) == ch || tolower(ch) == top)) {
+			polymerStack.pop();
+		}
+		else {
+			polymerStack.push(ch);
+		}
+	}
+	return polymerStack.size();
+}
 
 int main(int argc, char *argv[]) {
 	string input;
@@ -24,22 +46,30 @@ int main(int argc, char *argv[]) {
 		std::cin >> input;
 	}
 
-	std::stack<char> polymerStack;
+	auto units = calculatePolymerUnits(input);
 
+	std::cout << "Polymer units: " << units << '\n';
+
+	std::unordered_set<char> letters;
 	for (const char ch : input) {
-		if (polymerStack.empty()) {
-			polymerStack.push(ch);
-			continue;
-		}
-		char top = polymerStack.top();
-		if (top != ch && (tolower(top) == ch || tolower(ch) == top)) {
-			polymerStack.pop();
-		}
-		else {
-			polymerStack.push(ch);
+		letters.emplace(tolower(ch));
+	}
+
+	size_t minPolymerUnits = std::numeric_limits<size_t>::max();
+	char optimumLetter = '0';
+	for (const char letter : letters) {
+		string copyInput{ input };
+		copyInput.erase(std::remove_if(copyInput.begin(),
+			copyInput.end(),
+			[&](unsigned char x) { return x == letter || tolower(x) == letter; }),
+			copyInput.end());
+		auto units = calculatePolymerUnits(copyInput);
+		if (units < minPolymerUnits) {
+			minPolymerUnits = units;
+			optimumLetter = letter;
 		}
 	}
 
-	std::cout << "Polymer units: " << polymerStack.size() << '\n';
+	std::cout << "Minimum units found: " << minPolymerUnits << " removing letter: " << optimumLetter << '\n';
 	return 0;
 }
